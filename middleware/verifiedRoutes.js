888b79593b4fbaf518e0dd1439ea8eeebf000713
Toniddarden1 <Toniddarden@gmail.com>
2020-03-02 22:35:18 -0500

    added delete route

--- middleware/verifiedRoutes.js
+++ middleware/verifiedRoutes.js
@@ -1,11 +1,21 @@
-module.exports = (role) =>  {
-    return function (req, res, next) {
-        if(req.decodedJwt.roles && req.decodedJwt.roles.includes(role)){
-            next();
-        } else if (req.decodedJwt.roles && req.decodedJwt.roles.includes('Admin')){
-            next();
-        } else {
-            res.status(403).json({message: 'You are missing ADMIN permissions to continue'})
-        }
+// module.exports = (role) =>  {
+//     return function (req, res, next) {
+//         if(req.decodedJwt.roles && req.decodedJwt.roles.includes(role)){
+//             next();
+//         } else if (req.decodedJwt.roles && req.decodedJwt.roles.includes('Admin')){
+//             next();
+//         } else {
+//             res.status(403).json({message: 'You are missing ADMIN permissions to continue'})
+//         }
+//     }
+// }
+
+module.exports = verifyRoutes = (req, res, next) => {
+    const { userID } = req.params;
+
+    if (userID != req.user.id) {
+      res.status(401).json({ message: 'Unauthorized Access!' });
+    } else {
+      next();
     }
-}
+  };
--- users/userRoute.js
+++ users/userRoute.js
@@ -1,105 +1,129 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const genToken = require('../auth/generateToken');
-const vRoute = require('../middleware/verifiedRoutes');
-const vAccess = require('../middleware/verifiedAccess');
+const genToken = require("../auth/generateToken");
+const vRoute = require("../middleware/verifiedRoutes");
+const vAccess = require("../middleware/verifiedAccess");
 // const vToken = require('../middleware/verifiedToken');
 
-const User = require('../users/model');
+const User = require("../users/model");
 
 // POST
 
-router.post('/register', async (req, res) => {
-  console.log('firstname', req.body.firstname);
-    let body = req.body;
+router.post("/register", async (req, res) => {
+  console.log("firstname", req.body.firstname);
+  let body = req.body;
 
-    let { firstname, lastname, email, username, password } = body;
-    if (!firstname || !lastname || !email || !username || !password) {
-      res.status(400).json({ message: 'Please fill out all fields: Required' });
-    }
+  let { firstname, lastname, email, username, password } = body;
+  if (!firstname || !lastname || !email || !username || !password) {
+    res.status(400).json({ message: "Please fill out all fields: Required" });
+  }
 
-    const hash = bcrypt.hashSync(password, 12);
-    body.password = hash;
-  
-    try {
-      const user = await User.addUser(body);
-      const token = genToken(user);
-      res.status(201).json({ user, token });
+  const hash = bcrypt.hashSync(password, 12);
+  body.password = hash;
 
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-
-  // LOGIN
-
-router.post('/login', (req, res) => {
-  let {username, password} = req.body;
-
-  User.findBy({username})
-  .first()
-  .then(user => {
-      if(user && bcrypt.compareSync(password, user.password)){
-          const token = genToken(user);
-
-          res.status(200).json({username: user.username, token: token})
-      } else {
-          res.status(401).json({message: "invalid credits"})
-      }
-  }).catch(error => {
-      res.status(500).json(error);
-  })
-
-})
-
-
-// for my own testing below
- router.get('/:id', async (req, res) => {
-
-  User.findById(req.params.id)
-  .then(user => {
-    if(user){
-      res.status(200).json({success:true, user})
-    } else {
-      res.status(404).json({ message: "The post with the specified ID does not exist."})
-    }
-  })
-  .catch(err => {
-    res.status(500).json({error: "The post information could not be retrieved.", error:err});
-  })
+  try {
+    const user = await User.addUser(body);
+    const token = genToken(user);
+    res.status(201).json({ user, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
+// LOGIN
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  User.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user);
+
+        res.status(200).json({ username: user.username, token: token });
+      } else {
+        res.status(401).json({ message: "invalid credits" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// GET/:id
+
+router.get("/:id", async (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json({ success: true, user });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          error: "The post information could not be retrieved.",
+          error: err
+        });
+    });
+});
 
 //  DELETE
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id",vAccess, async (req, res) => {
   const id = req.params.id;
   try {
     await User.deleteUser(id);
 
     res.status(200).json({
-      message: 'Your account has been deleted successfully '
+      message: "Your account has been deleted successfully "
     });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: 'Internal server Error' });
+    res.status(500).json({ message: "Internal server Error" });
   }
 });
 
+// PUT
 
-// -------
+router.put("/:id",vAccess,vRoute, async (req, res) => {
+  const id = req.params.id;
+  const { body } = req;
+  const { password } = body;
 
-// router.get("/all",vToken, vRoute('Admin'), (req, res) => {
-//   Users.find().then(users => {
-//       res.json(users)
-//   }).catch(error => {
-//       res.send(error);
-//       console.log(error)
-//   })
-// })
+  if (password) {
+    hash = bcrypt.hashSync(password, 12);
+    body.password = hash;
+  }
 
+  try {
+    const user = await User.updateUser(id, body);
+
+    const userChange = new Object({
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      username: user.username
+    });
+
+    res.status(201).json({
+      userChange,
+      message: "Account info updated successfully!!"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
